69f91a39eddca944ba3e531e603714eea6a76393
Zaid <1081161182+ZdAkk@users.noreply.github.com>
2022-11-17 10:29:26 +0100

    Started with work sheet 2

--- Sheet2/filteroperations.cpp
+++ Sheet2/filteroperations.cpp
@@ -45,12 +45,12 @@ QImage* filterImage(QImage * image, int**& filter, int filter_width, int filter_
             for(int v = -L; v <= L; v++ ){
                 for(int u = -K; u <= K; u++ ){
 
-                if ((i + v < 0) || (i + v > imageWidth)  || (j + u < 0) || (j + u > imageHeight)) {
-                    sumCb = sumCb + (-128)*filter[v + L][u + K];
-                    sumCr = sumCr + (-128)*filter[v + L][u + K];
-                    continue;
-                }
-                     pixel = copyImage->pixel(i+v, j+u);
+                    if (((i + v) < 0) || ((i + v) > imageWidth)  || ((j + u) < 0) || ((j + u) > imageHeight)) {
+                        sumCb = sumCb + (-128)*filter[v + L][u + K];
+                        sumCr = sumCr + (-128)*filter[v + L][u + K];
+                        continue;
+                    }
+                    pixel = copyImage->pixel(i+v, j+u);
 
                     int gray = 0.299*qRed(pixel) + 0.587*qGreen(pixel) + 0.114*qBlue(pixel);
                     int cb = -0.169*qRed(pixel) + -0.331*qGreen(pixel) + 0.5*qBlue(pixel);
@@ -198,6 +198,157 @@ QImage* filterImage(QImage * image, int**& filter, int filter_width, int filter_
      */
 QImage* filterGauss2D(QImage * image, double gauss_sigma, int border_treatment){
 
+    // create the kernel h
+    int center = (int) (3.0 * gauss_sigma);
+    float[] h = new float[2*center+1]; // odd size
+
+    //fill the kernel
+    double gauss_sigma2 = gauss_sigma * gauss_sigma;
+
+    for (int i=0; i<h.length; i++) {
+        double r = center - i;
+        h[i] = (float) exp(-0.5*(r*r)/gauss_sigma2);
+    }
+
+
+    //  we have to iterate through h and ddetermine the length and set it to weight
+    QImage* copyImage =new QImage(*image);
+
+    int gauss_array_len_half = (h.length/2);
+    double weight = 1.0/(gauss_array_len_half);
+
+    int imageHeight = image->height();
+    int imageWidth = image->width();
+
+    for(int i=L;i<image->width()-L;i++){
+        for(int j=K;j<image->height()-K;j++){
+            QRgb pixel;
+            double sumGray = 0;
+            double sumCb = 0;
+            double sumCr = 0;
+
+            for(int v = ; v <= L; v++ ){
+
+            }
+
+
+            for(int v = -L; v <= L; v++ ){
+                for(int u = -K; u <= K; u++ ){
+
+                    if (((i + v) < 0) || ((i + v) > imageWidth)  || ((j + u) < 0) || ((j + u) > imageHeight)) {
+                        sumCb = sumCb + (-128)*filter[v + L][u + K];
+                        sumCr = sumCr + (-128)*filter[v + L][u + K];
+                        continue;
+                    }
+
+                    pixel = copyImage->pixel(i+v, j+u);
+
+                    int gray = 0.299*qRed(pixel) + 0.587*qGreen(pixel) + 0.114*qBlue(pixel);
+                    int cb = -0.169*qRed(pixel) + -0.331*qGreen(pixel) + 0.5*qBlue(pixel);
+                    int cr = 0.5*qRed(pixel) + -0.419*qGreen(pixel) - 0.08*qBlue(pixel);
+
+                    if (gray > 255) {
+                        gray = 255;
+                    }
+                    if (gray < 0) {
+                        gray = 0;
+                    }
+
+                    if (cb  < -128) {
+                        cb = -128;
+                    }
+
+                    if (cb > 127) {
+                        cb = 127;
+                    }
+
+                    if (cr  < -128) {
+                        cr = -128;
+                    }
+
+                    if (cr > 127) {
+                        cr = 127;
+                    }
+
+                    sumGray = sumGray + gray*filter[v + L][u + K];
+                    sumCb = sumCb + cb*filter[v + L][u + K];
+                    sumCr = sumCr + cr*filter[v + L][u + K];
+
+                }
+            }
+
+            int newGray = (int) round((double)sumGray * weight);
+            int newCb = (int) round((double)sumCb * weight);
+            int newCr = (int) round((double)sumCr * weight);
+
+            if (newGray < 0) {
+                newGray = 0;
+            }
+
+            if (newGray > 255) {
+                newGray = 255;
+            }
+
+            if (newCb  < -128) {
+                newCb = -128;
+            }
+
+            if (newCb > 127) {
+                newCb = 127;
+            }
+
+            if (newCr  < -128) {
+                newCr = -128;
+            }
+
+            if (newCr > 127) {
+                newCr = 127;
+            }
+
+            int rot = newGray + 45 * newCr / 32 ;
+            int gruen = newGray - (11 * newCb + 23 * newCr) / 32 ;
+            int blau = newGray + 113 * newCb / 64 ;
+
+            if (rot > 255) {
+                rot = 255;
+            }
+            if (rot < 0)  {
+                rot  = 0;
+            }
+            if (gruen > 255) {
+                gruen = 255;
+            }
+            if (gruen < 0)  {
+                gruen  = 0;
+            }
+            if (blau > 255) {
+                blau = 255;
+            }
+            if (blau < 0)  {
+                blau  = 0;
+            }
+            if (rot > 255) {
+                rot = 255;
+            }
+            if (rot < 0)  {
+                rot  = 0;
+            }
+            if (gruen > 255) {
+                gruen = 255;
+            }
+            if (gruen < 0)  {
+                gruen  = 0;
+            }
+            if (blau > 255) {
+                blau = 255;
+            }
+            if (blau < 0)  {
+                blau  = 0;
+            }
+
+            image->setPixel(i, j, qRgb(rot,gruen,blau));
+        }
+    }
 
     logFile << "2D Gauss-Filter angewendet mit σ: " << gauss_sigma;
     logFile <<  " ---border treatment: ";
@@ -217,5 +368,6 @@ QImage* filterGauss2D(QImage * image, double gauss_sigma, int border_treatment){
     }
     return image;
 }
+
 }
 
